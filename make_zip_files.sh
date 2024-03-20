e10ce06d33653cb4ac3beb1d071209a738b7496a
William Durand <will+git@drnd.me>
2024-03-20 12:36:40 +0100

    Remove all zip files before regenerating them

--- make_zip_files.sh
+++ make_zip_files.sh
@@ -2,17 +2,18 @@
 
 SOURCE_DIR=unpacked
 
+# Remove previous ZIP files, if any.
+rm -f dist/*.zip
+
 find "$SOURCE_DIR" -type d -maxdepth 1 -print0 | while read -r -d $'\0' folder; do
   if [[ "$folder" == "$SOURCE_DIR" ]]; then
     continue
   fi
 
   zip_file="$(basename "$folder").zip"
-  # Remove file if it exists.
-  rm -f "dist/$zip_file"
 
   # Move to the directory of the add-on and zip its content.
   pushd "$folder" || exit 1
-  zip "../../dist/$zip_file" *
+  zip "../../dist/$zip_file" ./*
   popd || exit 2
 done
