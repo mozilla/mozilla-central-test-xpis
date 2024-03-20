4c7fab0d3c6f2f7f609b6e4cb14096a10bc50173
William Durand <will+git@drnd.me>
2024-03-20 17:24:07 +0100

    Add support for privileged test extensions (#4)

--- make_zip_files.sh
+++ make_zip_files.sh
@@ -2,18 +2,22 @@
 
 SOURCE_DIR=unpacked
 
-# Remove previous ZIP files, if any.
-rm -f dist/*.zip
+for sub_folder in "regular" "privileged"; do
+  dist_folder="dist/$sub_folder"
 
-find "$SOURCE_DIR" -type d -maxdepth 1 -print0 | while read -r -d $'\0' folder; do
-  if [[ "$folder" == "$SOURCE_DIR" ]]; then
-    continue
-  fi
+  rm -rf "$dist_folder"
+  mkdir -p "$dist_folder"
 
-  zip_file="$(basename "$folder").zip"
+  find "$SOURCE_DIR/$sub_folder" -type d -maxdepth 1 -print0 | while read -r -d $'\0' folder; do
+    if [[ "$folder" == "$SOURCE_DIR/$sub_folder" ]]; then
+      continue
+    fi
 
-  # Move to the directory of the add-on and zip its content.
-  pushd "$folder" || exit 1
-  zip "../../dist/$zip_file" ./*
-  popd || exit 2
+    zip_file="$(basename "$folder").zip"
+
+    # Move to the directory of the add-on and zip its content.
+    pushd "$folder" || exit 1
+    zip "../../../$dist_folder/$zip_file" ./*
+    popd || exit 2
+  done
 done
